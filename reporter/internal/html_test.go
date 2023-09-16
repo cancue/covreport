f1eb368bcbcbb0f7e7ce42d8b94e9e74a231709e
kang <kanqmh@gmail.com>
2023-09-16 14:53:52 +0900

    test: add WriteHTML tests

--- reporter/internal/html_test.go
+++ reporter/internal/html_test.go
@@ -2,6 +2,7 @@ package internal_test
 
 import (
 	"bufio"
+	"fmt"
 	"strings"
 	"testing"
 
@@ -18,7 +19,9 @@ func TestReport(t *testing.T) {
 		err := gp.Report(nil)
 		assert.ErrorContains(t, err, `can't read "not-exist.go"`)
 	})
+}
 
+func TestWriteHTMLEscapedCode(t *testing.T) {
 	t.Run("should escape HTML symbols", func(t *testing.T) {
 		var buf strings.Builder
 		dst := bufio.NewWriter(&buf)
@@ -26,6 +29,40 @@ func TestReport(t *testing.T) {
 		assert.NoError(t, err)
 		err = dst.Flush()
 		assert.NoError(t, err)
+		assert.Equal(t, `&lt;&gt;&amp;&amp;    "&lt;&gt;&amp;&amp;    "`, buf.String())
+	})
+}
+
+func TestWriteHTMLEscapedLine(t *testing.T) {
+	ln := 3
+	code := "foo := 5"
+	uncoveredCount := 0
+	coveredCount := 1
+
+	t.Run("should have class by the count", func(t *testing.T) {
+		var tests = []struct {
+			count *int
+			class string
+		}{
+			{nil, ""},
+			{&uncoveredCount, " uncovered"},
+			{&coveredCount, " covered"},
+		}
+
+		for _, tc := range tests {
+			var buf strings.Builder
+			dst := bufio.NewWriter(&buf)
+			var count string
+			if tc.count != nil && *tc.count > 0 {
+				count = fmt.Sprintf("%dx", *tc.count)
+			}
+			expected := fmt.Sprintf(`<div class="line-number">%d</div><div class="covered-count%s">%s</div><pre class="line%s">%s</pre>%s`, ln, tc.class, count, tc.class, code, "\n")
+
+			err := internal.WriteHTMLEscapedLine(dst, ln, tc.count, code)
+			assert.NoError(t, err)
+			dst.Flush()
+			assert.Equal(t, expected, buf.String())
+		}
 	})
 }
 
